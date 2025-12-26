480c62b635c15ed97500350f14b305c452cd1540
Atharv Kulkarni <atharvacoolkarni24@gmail.com>
2025-12-26 10:51:06 +0530

    Initial commit:chat app with CRUD operations

---
+++ index.js
@@ -0,0 +1,99 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const path=require("path");
+const Chat=require("./models/chat.js");
+const methodOverride=require("method-override");
+
+const port=8080;
+
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
+main().then(()=>{
+    console.log("connection successful");
+}).catch((err)=>{
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+//Index Route
+app.get("/chats",async (req,res)=>{
+    let chats=await Chat.find();
+    res.render("index.ejs",{chats});
+});
+
+//new route
+app.get("/chats/new",(req,res)=>{
+    res.render("new.ejs");
+});
+
+app.post("/chats",(req,res)=>{
+    let {from,to,message}=req.body;
+    let newChat=new Chat({
+        from:from,
+        to:to,
+        message:message,
+        created_at:new Date()
+    });
+    newChat.save()
+    .then((res)=>{
+        console.log("chat was saved");
+    }).catch((err)=>{
+        console.log(err);
+    });
+    res.redirect("http://localhost:8080/chats");
+});
+
+
+//edit route
+app.get("/chats/:id/edit",async (req,res)=>{
+    let {id}=req.params;
+    let chat=await Chat.findById(id);
+    res.render("edit.ejs",{chat});
+});
+
+app.put("/chats/:id",async (req,res)=>{
+    let {id}=req.params;
+    let {newMessage}=req.body;
+    let updatedChat= await Chat.findByIdAndUpdate(id,{message:newMessage},{runValidators:true,new :true});
+    res.redirect("http://localhost:8080/chats");
+});
+
+
+//delete route
+
+app.delete("/chats/:id",async (req,res)=>{
+    let {id}=req.params;
+    let deletedChat=await Chat.findByIdAndDelete(id);
+    res.redirect("http://localhost:8080/chats");
+});
+
+// let chat1=new Chat({
+//     from:"Neha",
+//     to:"Priya",
+//     message:"send me your exam sheets",
+//     created_at:new Date(),
+// })
+
+// chat1.save().then((res)=>{
+//     console.log(res);
+// }).catch((err)=>{
+//     console.log(err);
+// });
+
+app.get("/",(req,res)=>{
+    res.send("root is working");
+});
+
+app.listen(port,()=>{
+    console.log(`Server is listening at ${port}`);
+});
